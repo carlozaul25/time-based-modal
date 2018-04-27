e0fd342068d476e34480e039cb96656cd85f90c0
carlozaul25 <carloz18rivera@live.com>
2018-04-26 22:46:35 -0400

    images

---
+++ index.js
@@ -0,0 +1,11 @@
+	
+			// var myVar;
+			// var popText = document.getElementById('popText').innerHTML;
+
+			function setTime() {
+   			 myVar = setTimeout(popBox, 3000);
+			}
+
+		function popBox() {
+  		document.getElementById('textHere').style.display= ''
+			}
