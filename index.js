ca6793ce71a07560e6223c6ee2f12e02bb0a6c0b
carlozaul25 <carloz18rivera@live.com>
2018-05-01 14:25:53 -0400

    modal-done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+var modal=document.getElementById('modal')
+var button=document.getElementById('btn')
+
+setTimeout(displayModel,5000)
+
+function displayModel(){
+ modal.style.display= "block"
+}
+
+button.addEventListener("click", function(){
+ modal.style.display= "none"
+})
 
